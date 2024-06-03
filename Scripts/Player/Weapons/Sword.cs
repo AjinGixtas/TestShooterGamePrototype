2fd6c8ac56d6a2da4e6a6219645459df7001a730
AjinGixtas <tdinhan999@gmail.com>
2024-06-04 04:50:34 +0700

    Minor number change

--- Scripts/Player/Weapons/Sword.cs
+++ Scripts/Player/Weapons/Sword.cs
@@ -1,15 +1,27 @@
 using Godot;
 
 public partial class Sword : Node2D {
-    [Export] float DAMAGE;
-    [Export] AnimationPlayer ANIMATION_PLAYER;
+    [Export] float NORMAL_SLASH_DAMAGE, SPECIAL_SLASH_DAMAGE, SLASH_CHARGE_THRESHOLD;
+    [Export] AnimationPlayer ANIMATION_PLAYER, PLAYER_ANIMATION_PLAYER;
     [Export] WeaponManager WEAPON_MANAGER; [Export] Player PLAYER;
     bool canAttack = true;
+    float currentSlashCharge = 0;
     int amountOfTargetHit = 0;
+    public void Charge(float delta) { 
+        if(delta > 0) { currentSlashCharge += delta; PLAYER_ANIMATION_PLAYER.Play("ChargeSwordSwing"); return; }
+        if(currentSlashCharge >= SLASH_CHARGE_THRESHOLD) { SpecialAttack(); return; }
+        Attack();
+    }
+    public void SpecialAttack() {
+        PLAYER_ANIMATION_PLAYER.Play("SwingNormalSword");
+        ANIMATION_PLAYER.Play("SwingNormalSword"); 
+    }
     public void Attack() {
         if(!canAttack) return; 
         PLAYER.slowDuration += 0.4f;
-        canAttack = false; ANIMATION_PLAYER.Play("SwingSword"); 
+        canAttack = false; 
+        PLAYER_ANIMATION_PLAYER.Play("SwingNormalSword");
+        ANIMATION_PLAYER.Play("SwingNormalSword"); 
     }
     public void OnAttackCooldownTimerTimeout() {
         canAttack = true;
